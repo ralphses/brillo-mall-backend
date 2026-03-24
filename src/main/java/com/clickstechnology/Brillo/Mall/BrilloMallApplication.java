c576476bd6feb1a0d3e2dee010bfde39756b5e96
Raphael Eze <eze.raph@gmail.com>
2026-03-24 07:40:31 +0100

    initial commit

---
+++ src/main/java/com/clickstechnology/Brillo/Mall/BrilloMallApplication.java
@@ -0,0 +1,13 @@
+package com.clickstechnology.Brillo.Mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrilloMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrilloMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clickstechnology/Brillo/Mall/BrilloMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickstechnology.Brillo.Mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrilloMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
